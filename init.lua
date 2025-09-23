20dc3b205141bbba10021b3e9afcee1476d95744
fdd <kristiyan.ivanov.98@gmail.com>
2025-09-23 13:24:11 +0300

    added flutter-tools and some formatting changes

--- init.lua
+++ init.lua
@@ -804,20 +804,21 @@ require('lazy').setup({
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, dart = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
+      format_on_save = false,
+      -- function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      --   local disable_filetypes = { c = true, cpp = true, dart = true }
+      --   if disable_filetypes[vim.bo[bufnr].filetype] then
+      --     return nil
+      --   else
+      --     return {
+      --       timeout_ms = 500,
+      --       lsp_format = 'fallback',
+      --     }
+      --   end
+      -- end,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
@@ -836,6 +837,12 @@ require('lazy').setup({
           command = 'clang-format',
           args = { '-style={BasedOnStyle: LLVM, IndentWidth: 4, TabWidth: 4, UseTab: Never, PointerAlignment: Left}' },
           stdin = true,
+        },
+        prettier = {
+          prepend_args = { '--tab-width', '4' },
+        },
+        prettierd = {
+          prepend_args = { '--tab-width', '4' },
         },
       },
     },
